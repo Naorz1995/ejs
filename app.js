ddc9ebce24a6a9f8887f2566e9db38b306ff504d
Naorz1995 <98359354+Naorz1995@users.noreply.github.com>
2022-07-17 17:02:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// const PORT = 3500;
+const PORT = process.env.PORT || 3500;
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+const path = require("path");
+const images = require('./modules/images')
+const publicDirectoryPath = path.join(__dirname, "./public");
+
+
+app.use(express.static(publicDirectoryPath));
+
+
+app.get("", (req, res) => {
+
+  res.render("index", {images: images});
+});
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Server is up on port " + PORT);
+});
+
